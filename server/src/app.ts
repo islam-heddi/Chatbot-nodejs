7532e9fc1c53a88b2a14a22be2c0d12eecec47f3
islam-heddi <islamheddi19@gmail.com>
2026-02-14 18:42:11 +0100

    backend: init the swagger docs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,33 @@
-import express from "express"
-import type { Request, Response } from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./db/db.js"
-dotenv.config()
-import userRoute from "./routes/user.routes.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+dotenv.config();
+import userRoute from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
+import path from "path";
+import YAML from "yamljs";
 
-const app = express()
-connectDB()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+const documentationFile = YAML.load(path.join("doc", "api.yml"));
+
+const app = express();
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.get("/api/", (_req: Request, res: Response) => {
-    res.send("hello islam");
-})
-app.use("/api/user", userRoute)
-
+  res.send("hello islam");
+});
+app.use("/api/user", userRoute);
+app.use("/api/doc", swaggerUI.serve, swaggerUI.setup(documentationFile));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`the server is listening on ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`the server is listening on ${PORT}`);
+  console.log(
+    `the documentation is available at http://localhost:${PORT}/api/doc`,
+  );
+});
 
-export default app
+export default app;
--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,53 +1,66 @@
 import { Request, Response } from "express";
 import { User } from "../models/User.js";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await User.find()
-        return res.status(200).json(users)
-    } catch (error) {
-        return res.status(500).json({ message: "Error fetching users" })
-    }
-}
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching users" });
+  }
+};
 
 export const registerUser = async (req: Request, res: Response) => {
-    const { username, email, password } = req.body
-    try {
-        const user = new User({ username, email, password })
-        const newUser = await user.save()
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 3600000
-        })
-        return res.status(201).json({ newUser, token })
-    } catch (error) {
-        return res.status(500).json({ message: "Error adding users", error: error.message })
-    }
-}
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    const newUser = await user.save();
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1h",
+      },
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 3600000,
+    });
+    return res.status(201).json({ newUser, token });
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ message: "Error adding users", error: error.message });
+  }
+};
 
 export const loginUser = async (req: Request, res: Response) => {
-    const { email, password } = req.body
-    try {
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(404).json({ message: "Email not found" })
-        }
-        const checkPassword = await (user as any).comparePassword(password)
-        if (!checkPassword) return res.status(401).json({ message: "Invalid password" })
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            maxAge: 3600000
-        })
-        return res.status(200).json({ user, token })
-    } catch (error) {
-        return res.status(500).json({ message: "Error logging in", error: error.message })
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Email not found" });
     }
-}
+    const checkPassword = await (user as any).comparePassword(password);
+    if (!checkPassword)
+      return res.status(401).json({ message: "Invalid password" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 3600000,
+    });
+    return res.status(200).json({ user, token });
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ message: "Error logging in", error: error.message });
+  }
+};
