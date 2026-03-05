d04efd2a3c0725aa7ce80bc1a82c40f59cc4b661
islam heddi <islamheddi19@gmail.com>
2026-03-05 14:34:20 +0100

    client(router): init routers + adding login and register, home router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <>Hello world</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
