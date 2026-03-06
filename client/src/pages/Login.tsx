109448f61f7bdc4b50d3a240c3e371dcf03c6b3f
islam heddi <islamheddi19@gmail.com>
2026-03-06 22:57:18 +0100

    client(design): add login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,29 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 function Login() {
+  const [email,setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
   return (
-    <div>
-      <div className="p-5 m-2 grid place-items-center h-screen border-2">
+    <div className="grid place-items-center h-screen">
+      <div className="p-5 m-2 border flex flex-col gap-4">
         <h1>Login</h1>
-        <input type="text" placeholder="enter your email"/>
-        <Button>Click me</Button>
+        <p>fill the information below to proceed</p>
+        <Label>Email</Label>
+        <Input type="email" placeholder="enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Label>Password</Label>
+        <Input type="password" placeholder="enter your password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <div className="flex flex-row gap-2">
+          <Button>Login</Button>
+          <Button onClick={() => {
+            setEmail("")
+            setPassword("")
+          }}>Reset</Button>
+        </div>
+        <p>If you dont have an account, press <Link to="/register" className="text-blue-500">here</Link></p>
       </div>
     </div>
   )
