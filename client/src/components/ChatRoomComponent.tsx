45def9048ad6e4e4396074dd8ede1b2237d74b35
islam heddi <islamheddi19@gmail.com>
2026-03-14 22:53:33 +0100

    client(component): optimized the history of chats

--- client/src/components/ChatRoomComponent.tsx
+++ client/src/components/ChatRoomComponent.tsx
@@ -1,7 +1,7 @@
 
 function ChatRoomComponent({name}: {name: string}) {
   return (
-    <div>{name}</div>
+    <div className="p-3 rounded-2xl mb-2 mt-2 mr-2 hover:bg-gray-100 cursor-pointer w-full">{name}</div>
   )
 }
 
