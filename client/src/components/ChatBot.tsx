b7e018fb32518d6ff0e536b32383aa472358284b
islam heddi <islamheddi19@gmail.com>
2026-03-14 10:53:48 +0100

    client(component): make the user message take 2/3 the width of the screen

--- client/src/components/ChatBot.tsx
+++ client/src/components/ChatBot.tsx
@@ -19,6 +19,12 @@ export default function ChatBot() {
   },{
     role: "System",
     message: "ok just send me your code"
+  },{
+    role:"User",
+    message: "Nulla laborum reprehenderit ullamco proident ad incididunt esse anim pariatur anim cillum. Commodo incididunt sint ullamco ut. Ea eu est sint labore exercitation quis laboris sint eu consequat. Lorem consectetur enim occaecat ea nisi aliquip et eiusmod reprehenderit mollit aute adipisicing ad.Sit consequat irure fugiat et esse minim. Consectetur commodo anim duis labore amet id. Ipsum ullamco non ex aliquip quis deserunt est cillum qui ea commodo. Proident minim excepteur culpa cupidatat eu qui laborum velit dolore id nisi dolore laborum incididunt."
+  },{
+    role:"System",
+    message: "Nulla laborum reprehenderit ullamco proident ad incididunt esse anim pariatur anim cillum. Commodo incididunt sint ullamco ut. Ea eu est sint labore exercitation quis laboris sint eu consequat. Lorem consectetur enim occaecat ea nisi aliquip et eiusmod reprehenderit mollit aute adipisicing ad.Sit consequat irure fugiat et esse minim. Consectetur commodo anim duis labore amet id. Ipsum ullamco non ex aliquip quis deserunt est cillum qui ea commodo. Proident minim excepteur culpa cupidatat eu qui laborum velit dolore id nisi dolore laborum incididunt."
   }]
   return (
     <div className="flex flex-col">
--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -3,7 +3,7 @@ function Message({role, message}: {
     message: string
 }) {
   return (
-    <div className={`inline-flex text-white ${role=="System"? "self-start bg-blue-500": "self-end bg-blue-200"} p-4 m-4 rounded-2xl`}><p>{message}</p></div>
+    <div className={`inline-flex text-white ${role=="System"? "self-start bg-blue-500": "self-end bg-blue-200 max-w-2/3"} p-4 m-4 rounded-2xl`}><p>{message}</p></div>
   )
 }
 
