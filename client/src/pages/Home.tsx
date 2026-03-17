2f74040ed1d06e6a343d753f6dc84bb6a8c2d4c2
Heddi Islam <islamheddi19@gmail.com>
2026-03-17 15:29:21 +0100

    client(page): update the home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="grid place-items-center h-screen">
+      <div className="rounded-2xl p-5 m-2 border flex flex-col gap-4">
+        <p>Welcome to the chatbot, ask what you want</p>
+        <p>start by</p>
+        <div className="flex flex-row flex-wrap gap-4">
+        <Link to={"/login"}><Button>Login</Button></Link>
+        <Link to={"/register"}><Button>Register</Button></Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
