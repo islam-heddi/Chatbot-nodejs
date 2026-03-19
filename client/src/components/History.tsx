77e4da2fd559ffe66281473a35fdb218902cbe65
Heddi Islam <islamheddi19@gmail.com>
2026-03-19 16:27:45 +0100

    client(zustand): add the Chat store

--- client/src/components/History.tsx
+++ client/src/components/History.tsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react/jsx-runtime";
 import ChatRoomComponent from "./ChatRoomComponent";
+import { Button } from "./ui/button";
 
 export default function History() {
   const Rooms = [{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "hello"},{id: 1, name: "New Channel"},{id: 1, name: "Javascript issue"}]
   //const Rooms: any[] = []
   // 
   return (
-    <div className="flex flex-col items-center w-2xl h-[90vh] border-r-2 max-w-2xs overflow-y-scroll">{Rooms.length < 1? <p className="justify-self-center">No Message passed yet</p>: <div className="w-full">{Rooms.map((value,index) => <Fragment key={index}><ChatRoomComponent name={value.name} /></Fragment>)}</div>}</div>
+    <div className="flex flex-col items-center w-2xl h-[90vh] border-r-2 max-w-2xs overflow-y-scroll"><Button className="m-2 w-2/3">New chat</Button>{Rooms.length < 1? <p className="justify-self-center">No Message passed yet</p>: <div className="w-full">{Rooms.map((value,index) => <Fragment key={index}><ChatRoomComponent name={value.name} /></Fragment>)}</div>}</div>
   )
 }
---
+++ client/src/context/Chat.tsx
@@ -0,0 +1,27 @@
+import {create} from "zustand"
+
+interface IMessage {
+  id: string,
+  role: "User" | "System",
+  message: string
+}
+
+type ChatType = {
+    chatId: string;
+    chatName: string;
+    messages: IMessage[];
+    updateChatId: (id: string) => void
+    updateChatName: (name: string) => void
+    updateMessages: (messages: IMessage[]) => void
+    addMessages: (message: IMessage) => void
+}
+
+export const useChat = create<ChatType>()((set, get) => ({
+    chatId: "",
+    chatName: "",
+    messages: [],
+    updateChatId: (id: string) => set({chatId: id}),
+    updateChatName: (name: string) => set({chatName: name}),
+    updateMessages: (messages: IMessage[]) => set({messages}),
+    addMessages: (message) => set({messages: [...get().messages, message]})  
+}))
